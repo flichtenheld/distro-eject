723e9e670a27de6d2d53cbe3bb6a00ae228ee668
Jeff Tranter <tranter@pobox.com>
2005-07-24 23:47:26 +0000

    Patch to support building on uClibc hosts that are not using gettext or i18n. Thanks to Ned Ludd <solar@gentoo.org>

--- i18n.h
+++ i18n.h
@@ -7,9 +7,11 @@
     程式國際化設計:  謝崑中
 */
 
+#include <features.h>
 #ifndef __i18n__
-   #define __i18n__
    #define PKG "eject"
+ # ifndef __UCLIBC__
+   #define __i18n__
    #define LOCALEDIR "/usr/share/locale"
 
    #include <locale.h>
@@ -17,7 +19,11 @@
    #define _(str) gettext (str)
    #define N_(str) (str)
    #define I18NCODE setlocale(LC_ALL,""); textdomain(PKG); bindtextdomain(PKG,LOCALEDIR);
-
+ #else
+   #define _(str) (str)
+   #define N_(str) (str)
+   #define I18NCODE
+ #endif
    void i18n_init (void);
 #endif
 
